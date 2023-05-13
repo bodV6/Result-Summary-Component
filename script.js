2f6b7aec50a4dd9222bf66294edc6caf4202723f
bodV6 <fairybod@gmail.com>
2023-05-14 00:24:23 +0100

    hml,vite,js setup

---
+++ script.js
@@ -0,0 +1,21 @@
+import data from "./data.json";
+
+function renderData(data) {
+  const container = document.getElementById("summary-details");
+  const html = data
+    .map(
+      (item) => `
+      <div class="item">
+        <img src=${item.icon}/>
+        <h4>${item.category}</h4>
+        <p>${item.score} / 100</p>
+      </div>
+    `
+    )
+    .join("");
+  container.innerHTML = html;
+}
+
+renderData(data);
+
+

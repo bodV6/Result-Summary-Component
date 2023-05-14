9eb66bc455314ba28138eb87a2dab952f2757ec2
bodV6 <fairybod@gmail.com>
2023-05-14 13:21:35 +0100

    Script updated

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 import data from "./data.json";
+import "./style.css";
 
 function renderData(data) {
   const container = document.getElementById("summary-details");
-  const html = data
-    .map(
-      (item) => `
-      <div class="item">
-        <img src="${item.icon}"/>
-        <h4>${item.category}</h4>
-        <p>${item.score} / 100</p>
-      </div>
-    `
-    )
-    .join("");
-  container.innerHTML = html;
+
+  data.forEach((item) => {
+    if (item.category === "Reaction") {
+      //   style.backgroundColor = red;
+    }
+
+    const div = document.createElement("div");
+    div.classList.add("indicators");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", item.icon);
+    div.appendChild(img);
+
+    const h4 = document.createElement("h4");
+    h4.setAttribute("style", "red")
+    const h4Text = document.createTextNode(item.category);
+    //   h4Text.setAttribute("style", "red");
+    h4.appendChild(h4Text);
+    div.appendChild(h4);
+
+    const p = document.createElement("p");
+    const pText = document.createTextNode(`${item.score} / 100`);
+    p.appendChild(pText);
+    div.appendChild(p);
+
+    container.appendChild(div);
+  });
 }
 
 renderData(data);
-
-
